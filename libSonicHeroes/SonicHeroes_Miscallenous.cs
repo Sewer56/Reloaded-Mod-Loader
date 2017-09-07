f7caba393ae39105299f62f66bd1a099a3f9f6df
sewer56lol <sewer56lol@googlemail.com>
2017-09-07 04:04:55 +0100

    Merge UsefulMethods into Miscallenous

--- libSonicHeroes/SonicHeroes_Miscallenous.cs
+++ libSonicHeroes/SonicHeroes_Miscallenous.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -7,6 +8,7 @@ using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SonicHeroes.Memory;
 
 namespace SonicHeroes.Misc
 {
@@ -185,6 +187,34 @@ namespace SonicHeroes.Misc
                 Y++;
             }
             return Bytes;
+        }
+
+        /// <summary>
+        /// This utility method will allow you to set a specified amount of bytes to 0x90/NOP in Assembly, disabling parts of code as wanted.
+        /// </summary>
+        /// <param name="NumberOfBytes">Number of bytes which you want to set to 0x90 (NOP/No Operation)</param>
+        /// <param name="Address">The address at which the NOP Operation Starts with.</param>
+        /// <param name="SonicHeroesProcess">The Process which holds the Sonic Heroes Game, generally Process.GetCurrentProcess()</param>
+        public static void NukeBytes(int NumberOfBytes, uint Address, Process SonicHeroesProcess)
+        {
+            byte[] Null = new byte[1] { 0x90 };
+            for (int x = 0; x < NumberOfBytes; x++)
+            {
+                SonicHeroesProcess.WriteMemory((IntPtr)Address + x, Null);
+            }
+        }
+
+        /// <summary>
+        /// Reads a byte array from a specified string of bytes e.g. 02 03 95 02 42
+        /// </summary>
+        /// <param name="String"></param>
+        /// <param name="Array"></param>
+        private byte[] Read_String_With_Byte_Array(string String)
+        {
+            string[] StringArray = String.Split(' ');
+            byte[] Array = new byte[StringArray.Length];
+            for (int x = 0; x < StringArray.Length; x++) { Array[x] = byte.Parse(StringArray[x]); }
+            return Array;
         }
     }
 }
