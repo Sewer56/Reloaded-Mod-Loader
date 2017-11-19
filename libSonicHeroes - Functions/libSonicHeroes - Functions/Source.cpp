5cc316bcabc441b7b94b511563b86920dab46d9d
sewer56lol <sewer56lol@googlemail.com>
2017-11-19 23:04:37 +0000

    libSonicHeroes - Functions | Small Cleanup

--- libSonicHeroes - Functions/libSonicHeroes - Functions/Source.cpp
+++ libSonicHeroes - Functions/libSonicHeroes - Functions/Source.cpp
@@ -6,7 +6,6 @@ using namespace std; // Might be useful, in my C++ improvement experience.
 // DLL Exports of Various Game Functions
 extern "C" __declspec(dllexport) void __stdcall Play_Song(int Song_Name_Pointer);
 extern "C" __declspec(dllexport) void __stdcall Play_Sound(int Sound_Effect_BANK_And_ID, int Unknown_Sound_Stream_Address, char Unknown_AlwaysZero, int Unknown_AlwaysZero_II);
-extern "C" __declspec(dllexport) void __stdcall Test_Method();
 
 // Pointer to function responsible for switching the current track/song.
 static const void *const Play_Song_Pointer = (void*)0x0043E7B0;
@@ -28,20 +27,28 @@ void __stdcall Play_Song(int Song_Name_Pointer)
 // Sound_Effect_BANK_And_ID = Bank Number as Char + Sound Number, 3 digits.
 // e.g. bank1 - Sound003 = 0x1003
 static const void* const Play_Sound_Pointer = (void*)0x004405F0;
-typedef int(__stdcall *Play_Sound_Function)(char Unknown_AlwaysZero, int Unknown_AlwaysZero_II);
+typedef void(__stdcall *Play_Sound_Function)(char Unknown_AlwaysZero, int Unknown_AlwaysZero_II);
 void __stdcall Play_Sound(int Sound_Effect_BANK_And_ID, int Unknown_Sound_Stream_Address, char Unknown_AlwaysZero, int Unknown_AlwaysZero_II)
 {
-	Play_Sound_Function Unknown_Method = (Play_Sound_Function)Play_Sound_Pointer;
-	__asm
+	try
 	{
-		mov esi, Unknown_Sound_Stream_Address
-		mov ebx, Sound_Effect_BANK_And_ID
+
+		Play_Sound_Function Unknown_Method = (Play_Sound_Function)Play_Sound_Pointer;
+		__asm
+		{
+			mov esi, Unknown_Sound_Stream_Address
+			mov ebx, Sound_Effect_BANK_And_ID
+		}
+		Unknown_Method(0, 0);
+		__asm
+		{
+			cmp eax, 0
+			je endlabel
+			mov[eax + 0x0C], 0x85
+			endlabel:
+		}
 	}
-	int Result = Unknown_Method(0, 0);
-	__asm
-	{
-		mov		[eax + 0x0C], 0x85
-	}
+	catch (exception ex) {} 
 }
 
 // Loads a level, maybe.
@@ -53,66 +60,74 @@ extern "C" __declspec(dllexport) void __stdcall Load_Level_Function(int Level_ID
 extern "C" __declspec(dllexport) void __stdcall Load_Test_Level_Function(int Level_ID);
 void __stdcall Load_Level_Function(int Level_ID)
 {
-	DWORD JumpAddress = 0x0402B49;
-	__asm
+	try
 	{
-		// Register Setup
-		mov eax, Level_ID
-		mov ebx, Level_ID
-		mov edi, 0x008D66E8
+		DWORD JumpAddress = 0x0402B49;
+		__asm
+		{
+			// Register Setup
+			mov eax, Level_ID
+			mov ebx, Level_ID
+			mov edi, 0x008D66E8
 
-		// Titlecard Function Call
-		mov [edi + 0x8], 1
-		mov [edi + 0x24], 0
-		mov [edi + 0x38], eax
-		mov [edi + 0x28], eax
-		
-		// The Function Call
-		push edi
-		call Load_Level_Pointer
+			// Titlecard Function Call
+			mov[edi + 0x8], 1
+			mov[edi + 0x24], 0
+			mov[edi + 0x38], eax
+			mov[edi + 0x28], eax
 
-		// Instructions After | New Setup
-		mov ecx, [edi + 0x28]
-		mov edx, [edi + 0x2C]
-		mov eax, [edx + ecx * 4]
-		mov [edi + 0x30], eax
-		mov [edi + 8], 2
+			// The Function Call
+			push edi
+			call Load_Level_Pointer
 
-		// Jump!
-		//jmp [JumpAddress]
+			// Instructions After | New Setup
+			mov ecx, [edi + 0x28]
+			mov edx, [edi + 0x2C]
+			mov eax, [edx + ecx * 4]
+			mov[edi + 0x30], eax
+			mov[edi + 8], 2
+
+			// Jump!
+			//jmp [JumpAddress]
+		}
 	}
+	catch (exception ex) {}
 }
 
 void __stdcall Load_Test_Level_Function(int Level_ID)
 {
-	DWORD JumpAddress = 0x0402B49;
-	__asm
+	try
 	{
-		// Register Setup
-		mov eax, Level_ID
-		mov ebx, Level_ID
-		mov edi, 0x008D66E8
+		DWORD JumpAddress = 0x0402B49;
+		__asm
+		{
+			// Register Setup
+			mov eax, Level_ID
+			mov ebx, Level_ID
+			mov edi, 0x008D66E8
 
-		// Titlecard Function Call
-		mov[edi + 0x8], 1
-		mov[edi + 0x24], 0
-		mov[edi + 0x38], eax
-		mov[edi + 0x28], eax
+			// Titlecard Function Call
+			mov[edi + 0x8], 1
+			mov[edi + 0x24], 0
+			mov[edi + 0x38], eax
+			mov[edi + 0x28], eax
 
-		// The Function Call
-		push edi
-		call Load_Level_Pointer
+			// The Function Call
+			push edi
+			call Load_Level_Pointer
 
-		// Instructions After | New Setup
-		mov ecx, 1
-		mov edx, [edi + 0x2C]
-		mov eax, [edx + ecx * 4]
-		mov[edi + 0x30], eax
-		mov[edi + 8], 2
+			// Instructions After | New Setup
+			mov ecx, 1
+			mov edx, [edi + 0x2C]
+			mov eax, [edx + ecx * 4]
+			mov[edi + 0x30], eax
+			mov[edi + 8], 2
 
-		// Jump!
-		//jmp [JumpAddress]
+			// Jump!
+			//jmp [JumpAddress]
+		}
 	}
+	catch (exception ex) {}
 }
 
 // Loads a level, maybe.
@@ -121,19 +136,48 @@ typedef int(__stdcall *Load_Level_Function_X2)();
 extern "C" __declspec(dllexport) int __stdcall Load_Level_Function_II(int Game_State_ID);
 int __stdcall Load_Level_Function_II(int Game_State_ID)
 {
-	__asm
+	try
 	{
-		mov esi, 0x00A81FF8
-		mov ecx, 0x008D66E8
-		mov edi, 0x008D66E8
-		mov eax, Game_State_ID
+		__asm
+		{
+			mov esi, 0x00A81FF8
+			mov ecx, 0x008D66E8
+			mov edi, 0x008D66E8
+			mov eax, Game_State_ID
 
-		//push esi
-		//push ecx
-		mov[edi + 0x8], eax
-		call Load_Level_Pointer_II
-		mov[edi + 0x8], 1
+			//push esi
+			//push ecx
+			mov[edi + 0x8], eax
+			call Load_Level_Pointer_II
+			mov[edi + 0x8], 1
+		}
 	}
+	catch (exception ex) {}
+
+	return 1;
+}
+
+// Loads a level, maybe.
+// Calls the game-state handler.
+extern "C" __declspec(dllexport) int __stdcall Change_Game_State(int Game_State_ID);
+int __stdcall Change_Game_State(int Game_State_ID)
+{
+	try
+	{
+		__asm
+		{
+			mov esi, 0x00A81FF8
+			mov ecx, 0x008D66E8
+			mov edi, 0x008D66E8
+			mov eax, Game_State_ID
+
+			//push esi
+			//push ecx
+			mov[edi + 0x8], eax
+			call Load_Level_Pointer_II
+		}
+	}
+	catch (exception ex) {}
 
 	return 1;
 }
@@ -169,6 +213,113 @@ Load_Debug_Function_X Load_Debug_Function_Method_Call;
 extern "C" __declspec(dllexport) void __stdcall Load_Debug_Function();
 void __stdcall Load_Debug_Function()
 {
-		Load_Debug_Function_Method_Call = (Load_Debug_Function_X)0x00419B00;
-		Load_Debug_Function_Method_Call((void*)0x008D6A04);
+	try
+	{
+		Load_Debug_Function_Method_Call = (Load_Debug_Function_X)0x00643E40;
+		Load_Debug_Function_Method_Call((void*)0x008DB5AC);
+	}
+	catch (exception ex) {}
+}
+
+
+// Loads a level, maybe.
+// Calls the game-state handler.
+typedef int(__cdecl *Load_AFS_Language_File_X)();
+Load_AFS_Language_File_X Load_AFS_Language_File_Call;
+extern "C" __declspec(dllexport) void __stdcall Load_AFS_Language_File();
+void __stdcall Load_AFS_Language_File()
+{
+	Load_AFS_Language_File_Call = (Load_AFS_Language_File_X)0x0043E2F0;
+	Load_AFS_Language_File_Call();
+}
+
+// Pointer to function responsible for switching the current track/song.
+static const void *const Load_File_Into_Memory_Function_Pointer = (void*)0x00446100;
+extern "C" __declspec(dllexport) void __stdcall Load_File_Into_Memory(int Pointer);
+void __stdcall Load_File_Into_Memory(int Pointer)
+{
+	__asm
+	{
+		mov eax, Pointer
+		call Load_File_Into_Memory_Function_Pointer
+	}
+}
+
+// Pointer to function responsible for switching the current track/song.
+static const void *const Reload_Bank3_Pointer = (void*)0x004401C0;
+static const void *const Load_File_Pointer = (void*)0x00440A60;
+static const void *const Something_Pointer = (void*)0x004402B0;
+static const void *const Test = (void*)0x004400D0;
+
+extern "C" __declspec(dllexport) void __stdcall Reload_Bank3(int PointerX);
+void __stdcall Reload_Bank3(int PointerX)
+{
+	try 
+	{
+		__asm
+		{
+			mov eax, [PointerX]
+			call Test
+
+			/*
+			mov esi, 0x0FEF5AA0 // Unknown constant used in I/O operations.
+			push esi
+			call Load_File_Pointer
+			mov byte ptr[esi + 3], 0
+			mov byte ptr[esi + 4], 0
+			call Something_Pointer
+			call Reload_Bank3_Pointer
+			*/
+		}
+	}
+	catch (exception ex) {}
+}
+
+// Pointer to function responsible for switching the current track/song.
+static const void *const Test_Pointer_X = (void*)0x00427120;
+extern "C" __declspec(dllexport) void __stdcall Test_Method_X();
+void __stdcall Test_Method_X()
+{
+	try
+	{
+		__asm
+		{
+			//mov ecx, 0x008D66E8
+			mov esi, 0x00A81FF8
+			call Test_Pointer_X
+		}
+	}
+	catch (exception ex) {}
+}
+
+// Pointer to function responsible for switching the current track/song.
+static const void *const Exit_Stage = (void*)0x404640;
+extern "C" __declspec(dllexport) void __stdcall Exit_Stage_X();
+void __stdcall Exit_Stage_X()
+{
+	try
+	{
+		__asm
+		{
+			//mov ecx, 0x008D66E8
+			mov esi, 0x008D66E8
+			call Exit_Stage
+		}
+	}
+	catch (exception ex) {}
+}
+
+// Pointer to function responsible for switching the current track/song.
+static const void *const Experiment = (void*)0x00407260;
+extern "C" __declspec(dllexport) void __stdcall Experiment_X();
+void __stdcall Experiment_X()
+{
+	try
+	{
+		__asm
+		{
+			call Experiment
+		}
+	}
+	catch (exception ex) {}
 }
