61b064c2d2467b29a565cb76abc9ed45367b02be
sewer56lol <sewer56lol@googlemail.com>
2018-02-28 03:56:43 +0000

    First Commit

---
+++ Reloaded/Client.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reloaded.Networking;
+using Reloaded.Networking.MessageTypes;
+using static Reloaded.Networking.MessageTypes.LoaderServerMessages;
+
+namespace Reloaded_Mod_Template.Reloaded_Code
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class Client
+    {
+        /// <summary>
+        /// The client of the Mod Loader Server. 
+        /// Can be used for communication with the external local server (hosted in the CMD window).
+        /// </summary>
+        public static Reloaded.Networking.Client serverClient;
+
+        /// <summary>
+        /// Defines the message types that could be printed to the screen.
+        /// </summary>
+        public enum PrintMessageType
+        {
+            /// <summary>
+            /// Prints the message in the default white/gray colour.
+            /// </summary>
+            PrintText,
+
+            /// <summary>
+            /// Prints in the blue/information colour.
+            /// </summary>
+            PrintInfo,
+
+            /// <summary>
+            /// Prints in the yellow/orange warning colour.
+            /// </summary>
+            PrintWarning,
+
+            /// <summary>
+            /// Prints in the red/failure error colour.
+            /// </summary>
+            PrintError
+        }
+
+        /// <summary>
+        /// Sends a message to the mod loader server to print
+        /// a message to standard output.
+        /// </summary>
+        /// <param name="message">The text to print to the console.</param>
+        public static void Print(string message)
+        {
+            // Print in default style
+            Print(message, PrintMessageType.PrintText);
+        }
+
+        /// <summary>
+        /// Sends a message to the mod loader server to print
+        /// a message to standard output with a user defined style.
+        /// </summary>
+        /// <param name="message">The text to print to the console.</param>
+        /// <param name="printMessageType">The style of text to be printed to the screen.</param>
+        public static void Print(string message, PrintMessageType printMessageType)
+        {
+            // Get string to print as bytes.
+            byte[] bytesToSend = Encoding.ASCII.GetBytes(message);
+
+            // Build a Server Message with text to print.
+            // The style of the message is user-set.
+            Message.MessageStruct clientMessage = new Message.MessageStruct((ushort)MessageType.PrintText, bytesToSend);
+            
+            // Switch message type if necessary.
+            switch (printMessageType)
+            {
+                case PrintMessageType.PrintInfo: clientMessage.MessageType = (ushort)MessageType.PrintInfo; break;
+                case PrintMessageType.PrintError: clientMessage.MessageType = (ushort)MessageType.PrintError; break;
+                case PrintMessageType.PrintWarning: clientMessage.MessageType = (ushort)MessageType.PrintWarning; break;
+            }
+             
+            // Send the message.
+            serverClient.ClientSocket.SendData(clientMessage, true);
+        }
+    }
+}
---
+++ Reloaded/Initializer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Reloaded.GameProcess;
+using Reloaded_Mod_Template.Reloaded_Code;
+
+namespace Reloaded_Mod_Template
+{
+    public class Initializer
+    {
+        /// <summary>
+        /// This file contains the DLL Template for Reloaded Mod Loader mods.
+        /// If you are looking for user code, please see Program.cs
+        /// </summary>
+        /// <param name="portAddress">Stores the memory location of the port.</param>
+        [DllExport]
+        static void Main(int portAddress)
+        {
+            // Retrieve Assemblies from the "Libraries" folder.
+            AppDomain.CurrentDomain.AssemblyResolve += LocalAssemblyFinder.ResolveAssembly;
+
+            // Initialize
+            Init(portAddress);
+
+            // Call Init
+            Program.Init();
+        }
+
+        /// <summary>
+        /// This is here because of the logic of the CLR.
+        /// If the JIT tries to compile the method to execute, it will fail to find libreloaded,
+        /// before we even set the assembly resolution path with AppDomain.CurrentDomain.AssemblyResolve.
+        /// </summary>
+        /// <param name="portAddress">Stores the memory location of the port.</param>
+        static void Init(int portAddress)
+        {
+            // Set the game process.
+            Program.GameProcess = ReloadedProcess.GetCurrentProcess();
+
+            // Setup Local Server Client
+            Client.serverClient = new Reloaded.Networking.Client(IPAddress.Loopback, Program.GameProcess.ReadMemorySafe<int>((IntPtr)portAddress));
+            Client.serverClient.StartClient();
+        }
+    }
+}
