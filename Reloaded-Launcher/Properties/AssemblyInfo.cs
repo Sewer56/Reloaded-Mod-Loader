6be277a49f3dd46d2ab10897974d850953802600
sewer56lol <sewer56lol@googlemail.com>
2018-04-25 18:46:37 +0100

    [Art] Publish Reloaded Photoshop Original Artwork

--- Reloaded-Launcher/Properties/AssemblyInfo.cs
+++ Reloaded-Launcher/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@
 [assembly: System.Reflection.AssemblyCopyright("Sewer56lol, 2018")]
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.CLSCompliant(true)]
-[assembly: System.Reflection.AssemblyInformationalVersion("0.9.6.229977e")]
+[assembly: System.Reflection.AssemblyInformationalVersion("0.9.6.c02dcda")]
 [assembly: System.Reflection.AssemblyVersion("0.9.6")]
 [assembly: System.Reflection.AssemblyFileVersion("0.9.6")]
 
